bb210e0f78a4b94e467ca28a97edad4f853d590f
Bittu Gurrala <bittugurrala241@gmail.com>
2026-02-19 18:26:09 +0530

    added priority order and carried them to App parent

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react"
 import Tasks from "./components/Tasks"
 import TasksInputs from "./components/TasksInputs"
 
 function App() {
 
+  const[title, setTitle] = useState("")
+  const[desc, setDesc] = useState("")
+  const[prio, setPrio] = useState("")
+  const[tasks, setTasks] = useState([])
+
+  const submitHandler = (e)=>{
+    e.preventDefault()
+    console.log(`${title} ----- ${desc} ----  ${prio}`)
+    console.log(`form submitted`)
+  }
+
   return (
     <div className="h-screen w-screen flex md:flex-row flex-col align-start justify-between p-4">
-      <TasksInputs/>
+      <TasksInputs submitHandler = {submitHandler} title = {title} desc = {desc} prio = {prio} setTitle = {setTitle} setDesc= {setDesc} setPrio = {setPrio}/>
       <Tasks/>
     </div>
   )
--- src/components/TasksInputs.jsx
+++ src/components/TasksInputs.jsx
@@ -1,16 +1,16 @@
-export default function TasksInputs(){
+export default function TasksInputs({submitHandler, title, desc, prio, setTitle, setDesc, setPrio}){
     return (
         <div className="flex-1 p-4">
             <h3 className="text-3xl font-bold">Add To Do Tasks here</h3>
-            <form className="my-7">
-                <input className = "border-2 rounded p-3 w-full" placeholder="Enter a Task"></input>
-                <textarea className = "border-2 rounded p-3 w-full my-4 h-60" placeholder="Description"></textarea>
+            <form className="my-7" onSubmit={submitHandler}>
+                <input className = "border-2 rounded p-3 w-full" value = {title} onChange = {(e) => setTitle(e.target.value)} placeholder="Enter a Task"></input>
+                <textarea className = "border-2 rounded p-3 w-full my-4 h-60" value = {desc} onChange = {(e) => setDesc(e.target.value)} placeholder="Description"></textarea>
                 <div className="flex justify-between my-5">
-                    <label className="flex justify-center gap-2 text-red-500"><input type="radio" name="gender" value="3"/>High Priority</label>
-                    <label className="flex justify-center gap-2 text-orange-300"><input type="radio" name="gender" value="2"/>Medium Priority</label>
-                    <label className="flex justify-center gap-2 text-green-300"><input type="radio" name="gender" value="1"/>Least Priority</label>
+                    <label className="flex justify-center gap-2 text-red-500"><input type="radio" name="gender" value="3" onChange = {(e) => setPrio(e.target.value)}/>High Priority</label>
+                    <label className="flex justify-center gap-2 text-orange-300"><input type="radio" name="gender" value="2" onChange = {(e) => setPrio(e.target.value)}/>Medium Priority</label>
+                    <label className="flex justify-center gap-2 text-green-300"><input type="radio" name="gender" value="1" onChange = {(e) => setPrio(e.target.value)}/>Least Priority</label>
                 </div>
-                <button className="rounded bg-white text-black my-8 w-full">Add</button>
+                <button className="rounded bg-white text-black my-8 w-full active:bg-gray-500">Add</button>
             </form>
         </div>
     )
